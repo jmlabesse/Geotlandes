1d8d4069235be512ab7341235a5041bf8c649aa4
jmlabesse <jeanmarie.labesse@gmail.com>
2017-03-09 18:18:56 +0100

    Ajout js pour fond modal

---
+++ fond_modal.js
@@ -0,0 +1,11 @@
+var btn = document.getElementById('btn-nav');
+var modal = document.getElementById('fond-modal');
+var nav = document.getElementById('barre-nav');
+
+btn.addEventListener('click', function (e) {
+    $(nav).toggleClass("top-z");
+    $(modal).toggleClass("masque");
+    $(modal).toggleClass("affiche");
+
+});
+
